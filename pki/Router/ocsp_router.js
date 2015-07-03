eb254a22f7b3706e45bdeb3fd42c1e3b348437f9
root <root@a51a32d414b2.(none)>
2015-07-03 12:35:42 +0000

    First Commit

---
+++ pki/Router/ocsp_router.js
@@ -0,0 +1,48 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+var chokidar = require('chokidar');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+        console.log(req.headers["content-type"]);
+if(req.method == "POST" && req.headers["content-type"] == "application/ocsp-request"){
+proxy.web(req, res, { target: 'http://127.0.0.1:8888' });
+}else{
+proxy.web(req, res, { target: 'http://127.0.0.1:8080' });
+}
+});
+
+console.log("listening on port 3000")
+server.listen(3000);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(8080);
+
+chokidar.watch('/opt/pki/ca/index.txt', {ignored: /[\/\\]\./}).on('change', function(event, path) {
+  fs.appendFile('/var/log/ocsp-router/ocsp-router.log',event+":"+path.ctime+"\n", function (err) {
+});
+console.log(event,path);
+exec("/etc/init.d/ocsp stop");
+
+});
+
+
+
